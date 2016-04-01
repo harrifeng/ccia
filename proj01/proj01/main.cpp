2a18199c54772cd386133d9e935943c244d48718
Harri Feng <harri.feng@gmail.com>
2016-04-01 22:48:15 +0800

    Some try

--- proj01/proj01/main.cpp
+++ proj01/proj01/main.cpp
@@ -6,7 +6,16 @@ void hello()
 	std::cout << "Hello Concurrent World\n";
 }
 
+void world()
+{
+
+	std::cout << "World is here\n";
+}
 int main() {
 	std::thread t(hello);
 	t.join();
+
+
+	std::thread t2(world);
+	t2.join();
 }
